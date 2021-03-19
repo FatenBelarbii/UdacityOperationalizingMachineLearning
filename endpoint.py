32f883138f9e8abc5c6077f343fdc139b4d424b0
FatenBelarbii <fatenbelarbi@gmail.com>
2021-03-19 17:26:55 +0000

    Add files via upload

--- endpoint.py
+++ endpoint.py
@@ -3,9 +3,9 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = 'http://dcaca8d6-f0b3-479e-9ff8-2ef3771581ea.southcentralus.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = ''
+key = 'WcrKV6gzjbg7TnVnx1LYaRIbQWmqBwe2'
 
 # Two sets of data to score, so we get two results back
 data = {"data":
